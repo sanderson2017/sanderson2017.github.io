3ae6197a3347cf607de4691422e68c81c5711664
sanderson2017 <sanderson2017@kellogg.northwestern.edu>
2016-02-07 23:50:33 -0600

    Final Project Submission
    Website and Java list

---
+++ makeupchecklist.js
@@ -0,0 +1,50 @@
+// This code runs when the page loads
+
+
+$(function() {
+	$(document).ready(function(){
+    $("button").click(function(){
+        $(".foundation").remove();
+    });
+});
+})
+
+$(function() {
+	$(document).ready(function(){
+    $("button").click(function(){
+        $(".concealer").remove();
+    });
+});
+})
+
+$(function() {
+	$(document).ready(function(){
+    $("button").click(function(){
+        $(".blush").remove();
+    });
+});
+})
+
+$(function() {
+	$(document).ready(function(){
+    $("button").click(function(){
+        $(".bronzer").remove();
+    });
+});
+})
+
+$(function() {
+	$(document).ready(function(){
+    $("button").click(function(){
+        $(".lipstick").remove();
+    });
+});
+})
+
+$(function() {
+	$(document).ready(function(){
+    $("button").click(function(){
+        $(".lashes").remove();
+    });
+});
+})
